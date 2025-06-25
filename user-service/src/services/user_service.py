ae3ec9fb8d6b1944543806b18e4ba4a446d54e58
imbalet <zhmaevva@gmail.com>
2025-06-25 22:35:49 +0300

    fix(auth/serv): fix returning None and working with UUID

--- user-service/src/services/user_service.py
+++ user-service/src/services/user_service.py
@@ -1,4 +1,5 @@
 from typing import cast
+from uuid import UUID
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
 from src.schemas.user import UserResponse, UserInDB
@@ -21,15 +22,20 @@ class UserService:
             await session.refresh(new_user)
             return UserResponse.model_validate(new_user, from_attributes=True)
 
-    async def get_user(self, user_id: str) -> UserInDB:
+    async def get_user(self, user_id: UUID) -> UserInDB | None:
         async with self.session_factory() as session:
             session = cast(AsyncSession, session)
             result = await session.get(UserOrm, user_id)
+            if result is None:
+                return None
             return UserInDB.model_validate(result, from_attributes=True)
 
-    async def get_user_by_email(self, email: str) -> UserInDB:
+    async def get_user_by_email(self, email: str) -> UserInDB | None:
         async with self.session_factory() as session:
             session = cast(AsyncSession, session)
             stmt = select(UserOrm).where(UserOrm.email == email)
             result = await session.execute(stmt)
-            return UserInDB.model_validate(result.scalar(), from_attributes=True)
+            res = result.scalar()
+            if res is None:
+                return None
+            return UserInDB.model_validate(res, from_attributes=True)
