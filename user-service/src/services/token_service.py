048977634c772e267d76f5bc74bed60c963d284c
imbalet <zhmaevva@gmail.com>
2025-06-27 23:04:17 +0300

    feat(auth/serv): add exception handling on insert token

--- user-service/src/services/token_service.py
+++ user-service/src/services/token_service.py
@@ -4,9 +4,11 @@ from uuid import UUID
 
 from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
+from sqlalchemy.exc import IntegrityError
 
 from src.schemas import RefreshTokenData
 from src.models import RefreshTokensOrm
+from src.exceptions import NotFoundError, AlreadyExistsError, Entity
 
 
 class RefreshTokenService:
@@ -17,18 +19,28 @@ class RefreshTokenService:
     async def save(
         self, jti: UUID, token_hash: str, expiration_time: datetime, user_id: UUID
     ) -> RefreshTokenData:
-        async with self.session_factory() as session:
-            session = cast(AsyncSession, session)
-            new_token = RefreshTokensOrm(
-                jti=jti,
-                user_id=user_id,
-                token_hash=token_hash,
-                expires_at=expiration_time,
-            )
-            session.add(new_token)
-            await session.commit()
-            await session.refresh(new_token)
-            return RefreshTokenData.model_validate(new_token, from_attributes=True)
+        try:
+            async with self.session_factory() as session:
+                session = cast(AsyncSession, session)
+                new_token = RefreshTokensOrm(
+                    jti=jti,
+                    user_id=user_id,
+                    token_hash=token_hash,
+                    expires_at=expiration_time,
+                )
+                session.add(new_token)
+                await session.commit()
+                await session.refresh(new_token)
+                return RefreshTokenData.model_validate(new_token, from_attributes=True)
+        except IntegrityError as e:
+            orig_message = str(e.orig).lower()
+            if "duplicate" in orig_message:
+                raise AlreadyExistsError(
+                    Entity.TOKEN, message=f"Token with jti {jti} already exists"
+                )
+            raise NotFoundError(
+                entity=Entity.USER, message=f"User with id {user_id} not exists"
+            ) from e
 
     async def find_by_jti(self, jti: UUID) -> RefreshTokenData | None:
         async with self.session_factory() as session:
@@ -38,14 +50,14 @@ class RefreshTokenService:
                 return None
             return RefreshTokenData.model_validate(res, from_attributes=True)
 
-    async def revoke_token(self, jti: str) -> None:
+    async def revoke_token(self, jti: UUID) -> None:
         async with self.session_factory() as session:
             session = cast(AsyncSession, session)
             query = delete(RefreshTokensOrm).filter_by(jti=jti)
             await session.execute(query)
             await session.commit()
 
-    async def revoke_for_user(self, user_id: int) -> None:
+    async def revoke_for_user(self, user_id: UUID) -> None:
         async with self.session_factory() as session:
             session = cast(AsyncSession, session)
             query = delete(RefreshTokensOrm).filter_by(user_id=user_id)
