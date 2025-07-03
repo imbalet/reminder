c5c887eff676876af78bdcbcdf7c472996262f0c
imbalet <zhmaevva@gmail.com>
2025-07-03 17:21:35 +0300

    feat(rem): add alembic migrations, add status
    Add status for reminder using alembic

--- reminder-service/src/models.py
+++ reminder-service/src/models.py
@@ -1,8 +1,16 @@
+from enum import Enum
 from datetime import datetime, timezone
 from uuid import UUID, uuid4
 
 from sqlalchemy import DateTime, text
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+
+
+class Status(Enum):
+    PENDING = "pending"
+    PROCESSING = "processing"
+    SENT = "sent"
+    FAILED = "failed"
 
 
 class Base(DeclarativeBase):
@@ -15,8 +23,9 @@ class RemindersOrm(Base):
     id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
     title: Mapped[str]
     content: Mapped[str]
-    user_id: Mapped[UUID]
-    remind_date: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    user_id: Mapped[UUID] = mapped_column(index=True)
+    remind_date: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True)
+    status: Mapped[Status] = mapped_column(server_default="PENDING", index=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=text("TIMEZONE('utc', now())"),
