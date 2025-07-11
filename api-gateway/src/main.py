27484f67b2e2b0d31d268f95b6f57e668dbb41fb
imbalet <zhmaevva@gmail.com>
2025-07-11 18:38:35 +0300

    refactor(ag): divide main file

--- api-gateway/src/main.py
+++ api-gateway/src/main.py
@@ -1,33 +1,9 @@
-import logging
-
 from fastapi import FastAPI
 
 from src.api import auth_router, reminder_router, delivery_methods_router
-from src.config import config
+from src.logger import setup_logger
 
-logger = logging.getLogger(__name__)
-logger.setLevel(config.LOG_LEVEL.value)
-
-console_handler = logging.StreamHandler()
-console_handler.setLevel(config.LOG_LEVEL.value)
-file_handler = logging.FileHandler("app.log")
-file_handler.setLevel(config.LOG_LEVEL.value)
-
-
-formatter = logging.Formatter(
-    fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
-console_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
-
-logger.addHandler(console_handler)
-logger.addHandler(file_handler)
-
-
-logging.getLogger("uvicorn").handlers = logger.handlers
-logging.getLogger("uvicorn.access").handlers = logger.handlers
-logging.getLogger("fastapi").handlers = logger.handlers
+setup_logger()
 
 app = FastAPI(
     swagger_ui_parameters={
