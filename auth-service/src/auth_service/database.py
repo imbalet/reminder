3d1aa12c5997a3c6fdbe2492b57da9c272dc8d90
imbalet <zhmaevva@gmail.com>
2025-09-19 00:59:30 +0300

    refactor(auth): divide startup init

--- auth-service/src/auth_service/database.py
+++ auth-service/src/auth_service/database.py
@@ -1,6 +1,28 @@
 from auth_service.models import Base
+from auth_service.config import config
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 
 async def create_tables(engine):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+
+async def init_database():
+    engine = create_async_engine(
+        config.DB_URL,
+        echo=False,
+        pool_size=10,
+        max_overflow=20,
+        future=True,
+    )
+
+    AsyncSessionLocal = async_sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+        autoflush=False,
+    )
+    await create_tables(engine)
+
+    return engine, AsyncSessionLocal
--- auth-service/src/auth_service/startup_init.py
+++ auth-service/src/auth_service/startup_init.py
@@ -1,83 +1,47 @@
 from contextlib import asynccontextmanager
-from datetime import datetime, timezone
 import logging
 from pathlib import Path
 
 import aio_pika
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI
-from sqlalchemy import delete
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
 from auth_service.config import config
-from auth_service.database import create_tables
 from auth_service.services import SecurityService
-from auth_service.models import RefreshTokensOrm
+from auth_service.database import init_database
+from auth_service.tasks import setup_tasks
 
 logger = logging.getLogger(__name__)
 
 
-def get_channel_pool():
+def get_channel_pools():
     async def create_connection():
         return await aio_pika.connect_robust(config.RMQ_URL)
+
+    connection_pool = aio_pika.pool.Pool(create_connection, max_size=10)
 
     async def create_channel():
         async with connection_pool.acquire() as connection:
             return await connection.channel()
 
-    connection_pool = aio_pika.pool.Pool(create_connection, max_size=10)
     channel_pool = aio_pika.pool.Pool(create_channel, max_size=100)
-    return channel_pool
-
-
-async def delete_expired_tokens(session_factory: async_sessionmaker[AsyncSession]):
-    async with session_factory() as session:
-        try:
-            stmt = delete(RefreshTokensOrm).where(
-                RefreshTokensOrm.expires_at <= datetime.now(timezone.utc)
-            )
-            result = await session.execute(stmt)
-            await session.commit()
-            logger.info(f"Deleted {result.rowcount} expired tokens")
-        except Exception as e:
-            logger.error("Error deleting tokens", exc_info=e)
-            await session.rollback()
+    return connection_pool, channel_pool
 
 
 @asynccontextmanager
 async def startup_event(app: FastAPI):
-    engine = create_async_engine(
-        config.DB_URL,
-        echo=False,
-        pool_size=10,
-        max_overflow=20,
-        future=True,
-    )
-
-    AsyncSessionLocal = async_sessionmaker(
-        bind=engine,
-        class_=AsyncSession,
-        expire_on_commit=False,
-        autoflush=False,
-    )
-
-    await create_tables(engine)
-    app.state.session_factory = AsyncSessionLocal
+    app.state.db_engine, app.state.session_factory = await init_database()
     app.state.security_service = SecurityService(secret_path=Path(".secrets"))
-    app.state.channel_pool = get_channel_pool()
-    logger.info("DB started")
+    app.state.connection_pool, app.state.channel_pool = get_channel_pools()
 
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(
-        delete_expired_tokens,
-        "interval",
-        hours=3,
-        args=[app.state.session_factory],
-        next_run_time=datetime.now(timezone.utc),
-    )
-    scheduler.start()
+    delete_expired_tokens_task_scheduler = setup_tasks(app.state.session_factory)
+    logger.info("DB started")
 
     yield
 
-    scheduler.shutdown()
+    delete_expired_tokens_task_scheduler.shutdown()
+
+    await app.state.channel_pool.close()
+    await app.state.connection_pool.close()
+    await app.state.db_engine.dispose()
+
     logger.info("App stopped")
